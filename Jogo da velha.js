9a706602c5e87c9f20dfb1c1298a73d9620056f2
DavidSenai <silvadavidandrade625@gmail.com>
2024-04-29 15:46:30 -0300

    tiramos () do length

--- Jogo da velha.js
+++ Jogo da velha.js
@@ -72,7 +72,7 @@ function validaPosicao(entrada) {
     var entradaValida;
 
     entradaValida = false;
-    if (entrada.length() == 3) {
+    if (entrada.length == 3) {
         if (entrada.charAt(0) == "0" || entrada.charAt(0) == "1" || entrada.charAt(0) == "2") {
             if (entrada.charAt(2) == "0" || entrada.charAt(2) == "1" || entrada.charAt(2) == "2") {
                 entradaValida = true;
